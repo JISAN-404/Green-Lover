09bac3a08c829e35a924fc8676f30e90792dcd2f
MR DARK <105338726+JISAN-404@users.noreply.github.com>
2022-10-05 14:06:16 +0600

    Create Green.py

---
+++ Green.py
@@ -0,0 +1,49 @@
+import os, platform
+
+ 
+
+try:
+
+ 
+
+        import requests
+
+ 
+
+except:
+
+ 
+
+        os.system('pip2 install requests')
+
+ 
+
+ 
+
+ 
+
+import requests
+
+ 
+
+bit = platform.architecture()[0]
+
+ 
+
+if bit == "64bit":
+
+ 
+
+        import Green
+
+ 
+
+ 
+
+ 
+
+elif bit == "32bit":
+
+ 
+
+        import Green32
